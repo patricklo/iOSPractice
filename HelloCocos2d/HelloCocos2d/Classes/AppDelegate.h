e5b1958f155616d31dda363b39cdb5bfd4356c3f
Patrick Lo <patrick.jh.lo@gmail.com>
2014-12-16 18:08:19 +0800

    initialized commitment

---
+++ HelloCocos2d/HelloCocos2d/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  HelloCocos2d
+//
+//  Created by Patrick Lo on 12/16/14.
+//  Copyright Patrick Lo 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ HelloCocos2d/HelloCocos2d/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloCocos2d
+//
+//  Created by Patrick Lo on 12/16/14.
+//  Copyright Patrick Lo 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
