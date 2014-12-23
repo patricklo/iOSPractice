4c94f674121b0719d41b2f74c36b5c84f690d402
Patrick Lo <patrick.jh.lo@gmail.com>
2014-12-24 02:05:44 +0800

    AngryBird practice instialliazation

---
+++ AngryBird/AngryBird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AngryBird
+//
+//  Created by Patrick Lo on 12/24/14.
+//  Copyright (c) 2014 Patrick Lo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
